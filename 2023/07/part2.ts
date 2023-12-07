75f26c1aac0aae7cdc1c0031fce2dd82ff951be7
Ola Næss Kaldestad <mail@okal.no>
2023-12-07 11:04:25 +0100

    day 7

---
+++ 2023/07/part2.ts
@@ -0,0 +1,90 @@
+const file = await Deno.readTextFile("./input.txt");
+const start = performance.now();
+
+const cardStrengths = new Map<string, number>();
+for (const [i, card] of "J23456789TQKA".split("").entries()) {
+    cardStrengths.set(card, i);
+}
+
+const enum HandStrength {
+    HighCard,
+    Pair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+type Hand = {
+    cards: string;
+    strength: HandStrength;
+    bid: number;
+};
+
+function handStrength(cards: string) {
+    const counts = new Map<string, number>();
+
+    for (const card of cards.split("").filter(c => c !== "J")) {
+        counts.set(card, (counts.get(card) || 0) + 1);
+    }
+
+    const js = cards.split("").filter(c => c === "J").length;
+    const values = [...counts.values()].sort((a, b) => b - a);
+
+    if ((values[0] || 0) + js === 5) {
+        return HandStrength.FiveOfAKind;
+    }
+    else if (values[0] + js === 4) {
+        return HandStrength.FourOfAKind;
+    }
+    else if (values[0] + js === 3 && values[1] === 2) {
+        return HandStrength.FullHouse;
+    }
+    else if (values[0] + js === 3) {
+        return HandStrength.ThreeOfAKind;
+    }
+    else if (values[0] === 2 && values[1] === 2) {
+        return HandStrength.TwoPair;
+    }
+    else if (values[0] + js === 2) {
+        return HandStrength.Pair;
+    }
+
+    return HandStrength.HighCard;
+}
+
+const hands: Hand[] = [];
+for (const line of file.split("\n")) {
+    const [cards, bid] = line.split(" ");
+    
+    hands.push({
+        cards,
+        strength: handStrength(cards),
+        bid: parseInt(bid),
+    });
+}
+
+hands.sort((a, b) => {
+    if (a.strength !== b.strength) {
+        return b.strength - a.strength;
+    }
+
+    for (let i = 0; i < 5; ++i) {
+        if (a.cards[i] !== b.cards[i]) {
+            return cardStrengths.get(b.cards[i])! - cardStrengths.get(a.cards[i])!;
+        }
+    }
+
+    return 0;
+});
+
+let score = 0;
+for (const [i, hand] of hands.entries()) {
+    score += hand.bid * (hands.length - i);
+}
+
+const end = performance.now();
+
+console.log("answer:", score);
+console.log("time:", end - start, "ms");
