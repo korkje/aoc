76b4b12a50b7afe8681a160d023bfe8bfa014370
Ola Næss Kaldestad <mail@okal.no>
2023-12-22 13:03:50 +0100

    day 22

---
+++ 2023/22/part2.ts
@@ -0,0 +1,89 @@
+import read from "https://deno.land/x/read@v0.1.1/mod.ts";
+import FIFO from "https://deno.land/x/fifo@v0.2.2/mod.ts";
+
+const file = await read();
+const start = performance.now();
+
+const bricks: number[][][] = [];
+
+for (const line of file.split("\n")) {
+    bricks.push(line.split("~").map(_ => _.split(",").map(Number)));
+}
+
+bricks.sort((a, b) => a[0][2] - b[0][2]);
+
+const intersects = (a: number[][], b: number[][]) =>
+    Math.max(a[0][0], b[0][0]) <= Math.min(a[1][0], b[1][0]) &&
+    Math.max(a[0][1], b[0][1]) <= Math.min(a[1][1], b[1][1]);
+
+for (const [i, brick] of bricks.entries()) {
+    let maxZ = 1;
+
+    for (const check of bricks.slice(0, i)) {
+        if (intersects(brick, check)) {
+            maxZ = Math.max(maxZ, check[1][2] + 1);
+        }
+    }
+
+    brick[1][2] -= brick[0][2] - maxZ;
+    brick[0][2] = maxZ;
+}
+
+bricks.sort((a, b) => a[0][2] - b[0][2]);
+
+const supporter = new Map<number, Set<number>>();
+const supported = new Map<number, Set<number>>();
+
+for (let i = 0; i < bricks.length; ++i) {
+    supporter.set(i, new Set());
+    supported.set(i, new Set());
+}
+
+for (const [i, top] of bricks.entries()) {
+    for (const [j, bottom] of bricks.slice(0, i).entries()) {
+        if (intersects(bottom, top) && top[0][2] === bottom[1][2] + 1) {
+            supporter.get(j)!.add(i);
+            supported.get(i)!.add(j);
+        }
+    }
+}
+
+let total = 0;
+
+for (let i = 0; i < bricks.length; ++i) {
+    const quque = new FIFO<number>();
+    const falling = new Set<number>();
+
+    for (const j of supporter.get(i)!) {
+        if (supported.get(j)!.size === 1) {
+            quque.push(j);
+            falling.add(j);
+        }
+    }
+
+    falling.add(i);
+
+    while (quque.length) {
+        const j = quque.shift()!;
+
+        for (const k of supporter.get(j)!) {
+            if (falling.has(k)) {
+                continue;
+            }
+
+            const supportedBy = [...supported.get(k)!];
+
+            if (supportedBy.every(_ => falling.has(_))) {
+                quque.push(k);
+                falling.add(k);
+            }
+        }
+    }
+
+    total += falling.size - 1;
+}
+
+const end = performance.now();
+
+console.log("answer:", total);
+console.log("time:", end - start, "ms");
