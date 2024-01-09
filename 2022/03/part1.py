2911bc9314665fb9d87441e5b63cf6dc44fc8057
Ola Næss Kaldestad <mail@okal.no>
2024-01-09 17:42:00 +0100

    refactor

--- 2022/03/part1.py
+++ 2022/03/part1.py
@@ -2,14 +2,14 @@ from itertools import product
 
 lines = open(0).read().split("\n")
 abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-total = 0
 
-for line in lines:
+def priority(line):
     half = int(len(line) / 2)
 
     for left, right in product(line[:half], line[half:]):
         if left == right:
-            total += abc.index(left) + 1
-            break
+            return abc.index(left) + 1
+
+total = sum(map(priority, lines))
 
 print(total)
