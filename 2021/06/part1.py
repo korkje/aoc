1a1d6aa1e900ae4f68409f46f504cc981a19e0c3
Ola Næss Kaldestad <mail@okal.no>
2024-05-25 02:15:03 +0200

    day 6

---
+++ 2021/06/part1.py
@@ -0,0 +1,20 @@
+from functools import cache
+
+@cache
+def num_after_days(state, days = 80):
+    num = 1
+    next = state + 1
+
+    if days < next:
+        return num
+
+    remaining = days - next
+    for i in range(1 + remaining // 7):
+        num += num_after_days(8, remaining - 7 * i)
+
+    return num
+
+states = list(map(int, open(0).read().split(',')))
+total = sum(map(num_after_days, states))
+
+print(total)
