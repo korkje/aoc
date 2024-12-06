1beb1cbb274d6cb71c5b72005a162cfd576f18e8
Ola Næss Kaldestad <mail@okal.no>
2024-12-06 02:25:12 +0100

    day 2

---
+++ 2024/02/part1.ts
@@ -0,0 +1,45 @@
+import read from "jsr:@korkje/read";
+
+const file = await read();
+const start = performance.now();
+
+const isSafe = (nums: number[]): boolean => {
+    let dir: number | undefined;
+
+    for (let i = 0; i < nums.length - 1; ++i) {
+        const [a, b] = nums.slice(i, i + 2);
+        const diff = b - a;
+        const abs = Math.abs(diff);
+
+        if (dir === undefined) {
+            dir = Math.sign(diff);
+
+            if (dir === 0 || abs > 3) {
+                return false;
+            }
+
+            continue;
+        }
+
+        if (Math.sign(diff) !== dir || abs > 3) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+let safe = 0;
+
+for (const report of file.split("\n")) {
+    const nums = report.split(" ").map(Number);
+
+    if (isSafe(nums)) {
+        ++safe;
+    }
+}
+
+const end = performance.now();
+
+console.log("answer:", safe);
+console.log("time:", end - start, "ms");
